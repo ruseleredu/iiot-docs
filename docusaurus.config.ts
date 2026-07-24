79b56f67450ad033a9c96fa7cb5dd5ec66738122
Adriano Ruseler <ruseler@utfpr.edu.br>
2026-07-24 14:49:18 -0300

    Add docusaurus-search-local

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -175,6 +175,12 @@ const config: Config = {
         // ... other options specific to your API docs
       },
     ],
+    [
+      "@cmfcmf/docusaurus-search-local",
+      {
+        // Options here
+      },
+    ],
   ],
 
   themeConfig: {
